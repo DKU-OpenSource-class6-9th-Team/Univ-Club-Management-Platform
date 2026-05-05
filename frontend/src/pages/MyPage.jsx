25ef331b395a6f4c3301c11c4f5ac081bd1f8ce0
minjae1116 <minjae8780@gmail.com>
2026-05-05 15:39:56 +0900

    추가수정사항 반영1

---
+++ frontend/src/pages/MyPage.jsx
@@ -0,0 +1,10 @@
+function MyPage() {
+  return (
+    <div>
+      <h1>마이페이지</h1>
+      <p>사용자 정보 조회 화면입니다.</p>
+    </div>
+  );
+}
+
+export default MyPage;
---
+++ frontend/src/pages/ProfileEditPage.jsx
@@ -0,0 +1,10 @@
+function ProfileEditPage() {
+  return (
+    <div>
+      <h1>프로필 수정</h1>
+      <p>사용자 프로필 수정 화면입니다.</p>
+    </div>
+  );
+}
+
+export default ProfileEditPage;
