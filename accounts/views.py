20cb73deff147ab106930a0f404b0ec34353c518
minjae1116 <minjae8780@gmail.com>
2026-05-05 14:40:16 +0900

    KAN-14 accounts API 기본 연결 설정

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def accounts_api_home(request):
+    return Response({
+        "message": "accounts API is connected"
+    })
